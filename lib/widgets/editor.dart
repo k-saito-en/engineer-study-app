34285878ffbe2a879f8fd70c801e910ab1ad9930
k-saito-en <saito.kotaro.engineer@gmail.com>
2022-12-29 18:42:15 +0900

    # やったこと - pubspec.yaml   - flutter_markdown: ^0.6.13のインストール - editor.dart   - 編集画面にタブバーを実装   - 編集画面で入力したテキストをMD形式へパース、プレビュー画面に表示

--- lib/widgets/editor.dart
+++ lib/widgets/editor.dart
@@ -1,7 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 import '../providers.dart';
 
@@ -26,87 +29,107 @@ class Editor extends HookConsumerWidget {
     final textController = useTextEditingController();
     textController.text = note?.text ?? '';
 
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          if (note != null)
-            PopupMenuButton<String>(
-              initialValue: '',
-              icon: const Icon(Icons.more_vert),
-              // 削除処理の呼び出し
-              onSelected: (value) {
-                ref.read(currentNoteIdProvider.notifier).state = '';
-                ref.read(noteListProvider.notifier).delete(note.id);
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+              actions: [
+                if (note != null)
+                  PopupMenuButton<String>(
+                    initialValue: '',
+                    icon: const Icon(Icons.more_vert),
+                    // 削除処理の呼び出し
+                    onSelected: (value) {
+                      ref.read(currentNoteIdProvider.notifier).state = '';
+                      ref.read(noteListProvider.notifier).delete(note.id);
 
-                if (!isLargeScreen) {
-                  Navigator.pop(context);
-                }
-              },
-              itemBuilder: (BuildContext context) => [
-                const PopupMenuItem(
-                  child: Text('Delete'),
-                  value: 'delete',
-                )
+                      if (!isLargeScreen) {
+                        Navigator.pop(context);
+                      }
+                    },
+                    itemBuilder: (BuildContext context) => [
+                      const PopupMenuItem(
+                        value: 'delete',
+                        child: Text('Delete'),
+                      )
+                    ],
+                  ),
               ],
+              bottom: const TabBar(tabs: [
+                Tab(
+                  child: Text('preview'),
+                ),
+                Tab(
+                  child: Text('editing'),
+                )
+              ]),
             ),
-        ],
-      ),
-      // widgetの大きさが端末画面以上になる場合にスクロールできるようにするwidget
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // タイトル入力フィールドの設定
-            TextField(
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+            // widgetの大きさが端末画面以上になる場合にスクロールできるようにするwidget
+            body: TabBarView(children: [
+              // previewタブの実装
+              Container(
+                  margin: const EdgeInsets.all(20),
+                  child: Markdown(data: textController.text)),
+              // editingタブの実装
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      child: TextField(
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          contentPadding: EdgeInsets.only(
+                              left: 16, right: 16, top: 24, bottom: 4),
+                          hintText: 'Note name',
+                        ),
+                        controller: nameController,
+                        onChanged: (nextName) {
+                          ref
+                              .read(noteListProvider.notifier)
+                              .update(id: noteId, name: nextName);
+                        },
+                        enabled: note != null,
+                      ),
+                    ),
+                    // メモ本文入力フィールドの実装
+                    SizedBox(
+                      child: TextField(
+                        maxLines: null,
+                        style: const TextStyle(
+                          fontSize: 15,
+                          height: 1.5,
+                        ),
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          contentPadding: EdgeInsets.only(
+                              left: 16, right: 16, top: 4, bottom: 16),
+                          hintText: 'Note content',
+                        ),
+                        controller: textController,
+                        onChanged: (nextText) {
+                          ref
+                              .read(noteListProvider.notifier)
+                              .update(id: noteId, text: nextText);
+                        },
+                        enabled: note != null,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                focusedBorder: InputBorder.none,
-                enabledBorder: InputBorder.none,
-                errorBorder: InputBorder.none,
-                disabledBorder: InputBorder.none,
-                contentPadding:
-                    EdgeInsets.only(left: 16, right: 16, top: 24, bottom: 4),
-                hintText: 'Note name',
-              ),
-              controller: nameController,
-              onChanged: (nextName) {
-                ref
-                    .read(noteListProvider.notifier)
-                    .update(id: noteId, name: nextName);
-              },
-              enabled: note != null,
-            ),
-            // メモ本文入力フィールドの実装
-            TextField(
-              maxLines: null,
-              style: const TextStyle(
-                fontSize: 18,
-                height: 1.5,
-              ),
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                focusedBorder: InputBorder.none,
-                enabledBorder: InputBorder.none,
-                errorBorder: InputBorder.none,
-                disabledBorder: InputBorder.none,
-                contentPadding:
-                    EdgeInsets.only(left: 16, right: 16, top: 4, bottom: 16),
-                hintText: 'Note content',
-              ),
-              controller: textController,
-              onChanged: (nextText) {
-                ref
-                    .read(noteListProvider.notifier)
-                    .update(id: noteId, text: nextText);
-              },
-              enabled: note != null,
-            )
-          ],
-        ),
-      ),
-    );
+            ])));
   }
 }
